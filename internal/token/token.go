eb92b586da4a260c7bba7663921b7b39d81f377a
DBloodworth95 <dan.bloodworth95@gmail.com>
2025-03-15 00:28:19 +0000

    support more tokens in lexer

--- internal/token/token.go
+++ internal/token/token.go
@@ -22,9 +22,23 @@ const (
 	LET      = "LET"
 )
 
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
 type TokenType string
 
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+// LookupIndent returns a keyword TokenType
+// by mapping it from the ident string param
+func LookupIndent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
